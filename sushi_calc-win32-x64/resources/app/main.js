1906ddde87fbfba9f9433577af53d5e3369b1ea8
unknown <sushidev2@gmail.com>
2019-08-15 16:56:31 -0400

    added executable

---
+++ sushi_calc-win32-x64/resources/app/main.js
@@ -0,0 +1,41 @@
+//get modules
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+//create main window
+let window
+
+function createWindow () {
+    //window parameters
+    window = new BrowserWindow({
+        width: 600,
+        height: 700,
+        frame: false
+    })
+
+    window.loadFile('main.html');
+
+    //window closy bit
+    window.on('closed', () => {
+        window = null;
+    })
+
+}
+
+//when npm is ready to do stuff
+app.on('ready', createWindow)
+
+//auto quit if all windows are closed
+app.on('windows-all-closed', () => {
+    
+    //if you arent on mac, actually quit the program
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if(window === null) {
+        createWindow();
+    }
+})
